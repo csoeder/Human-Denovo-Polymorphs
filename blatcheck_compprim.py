aac0ea945b0d15b89befe4aa5523a280699fdf5f
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-15 11:15:50 -0400

    doublederp

--- blatcheck_compprim.py
+++ blatcheck_compprim.py
@@ -20,7 +20,7 @@ from Bio import SeqIO
 phial  = sys.argv[1]
 genome  = sys.argv[1]
 
-index = re.compile('blat(\d*).psl')	#	what does a BLAT result look like?
+index = re.compile('\w*blat(\d*).psl')	#	what does a BLAT result look like?
 number = index.match(phial).groups()[0]	#	scrape the ID number
 ##########################################	Slice and dice...
 os.system('sed 1,5d compprimBLATs/%s | grep chr[1-9,X,Y][0-9]*"\s" > compprimBLATs/%s.snipt'%tuple([phial, phial]))	#remove header; remove chrXXrandomY_blahblahblah hits
