3aa05654e3b211b1ded6b192e476ad4c5ccc0a5e
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-15 11:28:48 -0400

    monitor now logs git version

--- pipeline_v3.5.sh
+++ pipeline_v3.5.sh
@@ -11,7 +11,10 @@ touch monitor.log											#
 echo "de novo search begin: subject $1" >> monitor.log
 date >> monitor.log
 echo "RNA-Seq reads:	" >> monitor.log	#	Note the lack of Trinity assembly frontload
-du ERR* >> monitor.log
+du ERR* >> monitor.log								#
+echo "git commit #" >> monitor.log				#
+head .git/FETCH_HEAD | cut -f1 >> monitor.log #
+head .git/FETCH_HEAD | cut -f3 >> monitor.log 
 #############################################
 #Load relevant modules 						#
 . /nas02/apps/Modules/default/init/bash		#
