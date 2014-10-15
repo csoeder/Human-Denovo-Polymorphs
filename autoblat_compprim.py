7f614766469186401e94e47eca83e31c411e7328
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-15 10:08:30 -0400

    derp

--- autoblat_compprim.py
+++ autoblat_compprim.py
@@ -15,7 +15,7 @@ os.mkdir('compprimBLATs')		#	Open up the folder for output
 parser = SeqIO.parse(filein, 'fasta')	#	Parse the FASTA
 number=0										#	Initialize the count
 for rec in parser:						#	For each record in the FASTA...
-	SeqIO.write([rec], open('compprimBLATs/temp%s.fa'%n, 'w'), "fasta")	#	Write it.
+	SeqIO.write([rec], open('compprimBLATs/temp%s.fa'%number, 'w'), "fasta")	#	Write it.
 	call(['bsub', '-J', 'BLAT_%s_pan'%number, '-o', 'blat%s_pan.lsf.out'%number, 'blat', '/netscr/csoeder/1kGen/data/chimp/panTro4.fa','compprimBLATs/temp%s.fa'%item,  'compprimBLATs/pan_blat%s.psl'%number ])
 	call(['bsub', '-J', 'BLAT_%s_gor'%number, '-o', 'blat%s_gor.lsf.out'%number, 'blat', '/netscr/csoeder/1kGen/data/gorilla/gorGor3.fa','compprimBLATs/temps%s.fa'%item,  'compprimBLATs/gor_blat%s.psl'%number ])
 #	call (['blat', '/netscr/csoeder/1kGen/data/hg19.fa', 'BLATs/temp%s.fa'%n, 'BLATs/blat%s.psl'%n])
