90b5482e1d6c8c70242e3cca3a5c67be596dad90
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-15 11:08:21 -0400

    now with functionality

--- blatcheck_compprim.py
+++ blatcheck_compprim.py
@@ -21,11 +21,9 @@ phial  = sys.argv[1]
 genome  = sys.argv[1]
 
 index = re.compile('blat(\d*)\w*.psl')	#	what does a BLAT result look like?
-listing = os.listdir('BLATs')		#	Gather the BLAT results
-for item in listing:	#		For each one...
-	number = index.match(item).groups()[0]	#	scrape the ID number
-	call(['touch', 'BLATs/%s.snipt'%item])	#	Slice and dice...
-	os.system('sed 1,5d BLATs/%s | grep chr[1-9,X,Y][0-9]*"\s" > BLATs/%s.snipt'%tuple([item, item]))	#remove header; remove chrXXrandomY_blahblahblah hits
-	os.system('sort -k1,1 -r BLATs/%s.snipt > BLATs/%s.snipt.sortd'%tuple([item, item]))
-	os.system('head -n1 BLATs/%s.snipt.sortd > BLATs/%s.snipt.sortd.clipt'%tuple([item, item]))
+number = index.match(item).groups()[0]	#	scrape the ID number
+##########################################	Slice and dice...
+os.system('sed 1,5d compprimBLATs/%s | grep chr[1-9,X,Y][0-9]*"\s" > compprimBLATs/%s.snipt'%tuple([item, item]))	#remove header; remove chrXXrandomY_blahblahblah hits
+os.system('sort -k1,1 -r compprimBLATs/%s.snipt > compprimBLATs/%s.snipt.sortd'%tuple([item, item]))
+os.system('head -n1 compprimBLATs/%s.snipt.sortd > compprimBLATs/%s.snipt.sortd.clipt'%tuple([item, item]))
 
