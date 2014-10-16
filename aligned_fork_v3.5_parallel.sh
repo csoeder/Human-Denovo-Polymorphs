d03e41d588695a15480d9ee5834967d090ed288f
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-16 15:17:07 -0400

    derp

--- aligned_fork_v3.5_parallel.sh
+++ aligned_fork_v3.5_parallel.sh
@@ -119,6 +119,7 @@ mkdir compprimBLATs
 for candidate in `ls chunked_genes | grep exon`; do 
 	num=$(echo $candidate | cut -f 2 -d '_' |cut -f 1 -d '.');
 	region=$(head -n 1 chunked_genes/exons_$num.bed | cut -f 4);
+	echo $region is the best!
 	samtools faidx ../Trinity_files.Trinity.fasta $region > compprimBLATs/temp_$num.fa
 	python $SCRIPT_DIR/autoblat_compprim.py compprimBLATs/temp_$num.fa $4;
 done
