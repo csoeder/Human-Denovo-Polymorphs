a1f63d8304b0a6bea7e3d3bc92fd0c08d1b6b58e
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-24 18:02:35 -0400

    tuned up chromosome scan

--- dbquery_chromdist.py
+++ dbquery_chromdist.py
@@ -6,7 +6,7 @@ import string
 import csv
 import sys
 import os
-from numpy import arange
+from numpy import arange, mean, median
 from subprocess import call, check_output
 import matplotlib.pyplot as plt
 
@@ -15,23 +15,35 @@ conn = psycopg2.connect("dbname=denovogenes user=gene password=%s host=bioapps.i
 curr= conn.cursor()
 curr.execute('SELECT chrom, start, stop FROM location;')
 locations = curr.fetchall()
-chrm_dict={'X':0, 'Y':0}
+chrom_list = arange(1,23)
+chrom_list.extend(['X','Y'])
+chrm_dict={}
+for chrom in chrom_list:
+	chrm_dict['chr%s'%chrom]=0
 for place in locations:
 	if place[0] in chrm_dict.keys():
 		chrm_dict[place[0]] += 1
-	else:
-		chrm_dict[place[0]] = 1
 ###	Add in support for any empty chromosomes.
 
-plt.bar(arange(len(chrm_dict)), chrm_dict.values())
-plt.xticks(arange(len(chrm_dict)), chrm_dict.keys(), rotation=30)
+bars=[] 
+for i in chrom_list:
+	bars.append(chrm_dict[i])
+plt.bar(arange(len(chrm_dict)), bars)
+plt.xticks(arange(len(chrm_dict)), chrom_list, rotation=30)
 plt.xlabel('Chromosome')
 plt.ylabel('# genes')
 plt.title('Number of Polymorphic de novo Genes, by Chromosome')
 plt.savefig('Distribution_across_chromosomes.png')
 
 phial = open('Distribution_across_chromosomes.txt','w')
-phial.write('The %s genomic locations were '%tuple([len(locations)]))
+zero, nonzero = [], []
+for key in chrm_dict:
+	if chrm_dict[key] == 0:
+		zero.append(key)
+	else:
+		nonzero.append(key) 
+phial.write('The %s genomic locations were spread across %s chromosomes. %s chromosomes contained no sites.'%tuple([len(locations), len(nonzero), len(zero)]))
+phial.write('The least populated chromosome had %s sites. The most populated had %s. The mean and median were %s and %s sites, respectively.'%tuple([min(nonzero), max(nonzero), mean(nonzero), median(nonzero)]))
 phial.close()
 
 conn.commit()
