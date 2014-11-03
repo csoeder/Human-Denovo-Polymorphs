fe59e2e24af6a2aba21a5e9235822f75d39b8861
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-03 16:44:29 -0500

    cumulative novelty reporting

--- database_metaquery.sh
+++ database_metaquery.sh
@@ -8,12 +8,12 @@ python ../dbquery_basicfacts.py $pswd
 python ../dbquery_chromdist.py $pswd #	Please note! np.median returns an average if given an even number of data
 python ../dbquery_genesperperson.py $pswd
 python ../dbquery_peoplepergene.py $pswd
-
+python ../dbquery_cumulativenovelty.py $pswd
 
 
 #dbquery_associativity.py
 #dbquery_compareexpression.py
-#dbquery_cumulativenovelty.py
+#
 
 
 
