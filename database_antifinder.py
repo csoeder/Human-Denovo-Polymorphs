2ca173eb7e689acc888e474b626b637db5353da7
csoeder <charles.soeder@gmail.com>
2014-11-03 14:49:54 -0500

    worked on antifinder logic

--- database_antifinder.py
+++ database_antifinder.py
@@ -5,14 +5,14 @@ pwd = sys.argv[1]	#password
 conn = psycopg2.connect("dbname=denovogenes user=gene password=%s host=bioapps.its.unc.edu"%pwd)
 curr = conn.cursor()
 
-curr.execute("SELECT id FROM location;"%)
+curr.execute("SELECT id FROM location;")
 all_genes = curr.fetchall()
 
 for jean in all_genes:
 	curr.execute('SELECT pk FROM person WHERE person.pk IN (SELECT source FROM find WHERE find.loc != %s);'%jean[0])
 	nonexpressives = curr.fetchall()
 	for d00d in nonexpressives:
-		curr.execute("INSERT INTO antifind (source, loc) VALUES (%s, %s);", tuple([d00d, jean[0]])))
+		curr.execute("INSERT INTO antifind (source, seq, loc) VALUES (%s, '', %s);", tuple([d00d, jean[0]]))
 		conn.commit()
 
 curr.close()
