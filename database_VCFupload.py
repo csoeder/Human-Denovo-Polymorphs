07b47e1a25562dca4559110250764a869ae4832f
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-05 13:49:55 -0500

    unexpected indent

--- database_VCFupload.py
+++ database_VCFupload.py
@@ -14,23 +14,23 @@ curr = conn.cursor()
 curr.execute("SELECT id, chrom, start, stop FROM location;")
 loc_sites = curr.fetchall()
 
-	for site in loc_sites:
-		parser = vcf.Reader(open('/netscr/csoeder/1kGen/data/variation/ALL.chr%s.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz'%site[1][3:],'r'))
-		fetch = parser.fetch(loc_sites[1], loc_sites[2], loc_sites[3])
-		for rec in fetch:
-			alts = ''
-			hom_alts = ''
-			hom_refs = ''
-			hetz = ''			
-			for var in rec.ALT:
-				alts = '%s%s,'%tuple([alts, var])
-			for var in rec.get_hom_alts():
-				hom_alts = '%s%s,'%tuple([hom_alts, var.sample])
-			for var in rec.get_hom_refs():
-				hom_refs = '%s%s,'%tuple([hom_refs, var.sample])
-			for var in rec.get_hets():
-				hetz = '%s%s,'%tuple([hetz, var.sample])
-			curr.execute('INSERT INTO variant (chrom, pos, ref_allele, alt_alleles, hom_refs, hom_alts, hets, heterozygosity, is_snp, is_indel, pi_hat) VALUES (chr%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s);', tuple([rec.CHROM, rec.POS, rec.REF, alts, hom_refs, hom_alts, hetz, rec.heterozygosity, rec.is_snp, rec.is_indel, rec.nucl_diversity]))
+for site in loc_sites:
+	parser = vcf.Reader(open('/netscr/csoeder/1kGen/data/variation/ALL.chr%s.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz'%site[1][3:],'r'))
+	fetch = parser.fetch(loc_sites[1], loc_sites[2], loc_sites[3])
+	for rec in fetch:
+		alts = ''
+		hom_alts = ''
+		hom_refs = ''
+		hetz = ''			
+		for var in rec.ALT:
+			alts = '%s%s,'%tuple([alts, var])
+		for var in rec.get_hom_alts():
+			hom_alts = '%s%s,'%tuple([hom_alts, var.sample])
+		for var in rec.get_hom_refs():
+			hom_refs = '%s%s,'%tuple([hom_refs, var.sample])
+		for var in rec.get_hets():
+			hetz = '%s%s,'%tuple([hetz, var.sample])
+		curr.execute('INSERT INTO variant (chrom, pos, ref_allele, alt_alleles, hom_refs, hom_alts, hets, heterozygosity, is_snp, is_indel, pi_hat) VALUES (chr%s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s);', tuple([rec.CHROM, rec.POS, rec.REF, alts, hom_refs, hom_alts, hetz, rec.heterozygosity, rec.is_snp, rec.is_indel, rec.nucl_diversity]))
 
 
 
