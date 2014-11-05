e54494f40560dbee8794224dcd62fc93b9dc8da6
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-05 15:57:44 -0500

    type error fixed

--- database_lookback.py
+++ database_lookback.py
@@ -36,7 +36,7 @@ for find in all_finds:
 		start_dict[find[2]] = [find[1]]
 		curr.execute("SELECT seq FROM sequence WHERE sequence.id=%s"%find[3])	
 		seq = curr.fetchone()[0]
-		rex.append(SeqRecord(str(seq), find[2]))	#	create FASTA record with id = location pk; sequence = sequence
+		rex.append(SeqRecord(seq, str(find[2])))	#	create FASTA record with id = location pk; sequence = sequence
 SeqIO.write(rex, 'lookback.fasta', 'fasta')
 
 ###	2	:	align it to ALL the transcriptomes
