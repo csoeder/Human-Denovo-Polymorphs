c14578a36fe0e4b993e30b7d1660f098fb6ec37c
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-10 11:54:11 -0500

    close paren

--- pipeline_v3.5.sh
+++ pipeline_v3.5.sh
@@ -38,7 +38,7 @@ module load bwa 							#
 module load bowtie
 echo "$(date)	META:		modules loaded"	>> monitor.log
 #############################################
-if [[ ! -f Trinity_files.Trinity.fasta || ! -f $1_mapsplice_alignment.sam ]; then
+if [[ ! -f Trinity_files.Trinity.fasta || ! -f $1_mapsplice_alignment.sam ]]; then
 	for fastq in `grep $s dwnld_list.txt | cut -f 29`; do wget $fastq; gzip -d $fastq; done
 echo "RNA-Seq reads:	" >> monitor.log	#	Note the lack of Trinity assembly frontload
 du ERR* >> monitor.log								#
