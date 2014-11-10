d40e1c99706b043ff5c82344e8fa12a0978496d2
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-10 11:46:15 -0500

    reporting with WRITE

--- metapipe.sh
+++ metapipe.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+echo "w00tw00t we're piping" | write csoeder
 
 #######
 #For a steady submission rate of 3 per hour, which will finish in approx. 1 week
 nap_time=1200
 
+
 for folder in `cut -f 1 dwnld_list.txt | grep -v Source | sort | uniq`; do
+	echo "$folder queued" | write csoeder;
 	sh pipeline_v3.5.sh $folder > redirect_$folder.out;
+	echo "$folder submitted" | write csoeder;
 	sleep $nap_time;
 done
 
