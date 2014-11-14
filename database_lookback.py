feaee475a5122f687024d564173fb5604d7afbdd
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-14 13:48:21 -0500

    lookback 90% done

--- database_lookback.py
+++ database_lookback.py
@@ -37,24 +37,33 @@ for find in all_finds:
 		curr.execute("SELECT seq FROM sequence WHERE sequence.id=%s"%find[3])	
 		seq = curr.fetchone()[0]
 		phial.write('>%s\n'%str(find[2]))
-		phial.write('%s/n'%seq)
+		phial.write('%s\n'%seq)
 phial.close()
 
 ###	2	:	align it to ALL the transcriptomes
 
 for d00d in all_peeps:
 #	os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh | samtools sort - -f %s/%s_lookback.bam" &'%tuple([d00d[0]]*5))
-	os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh | samtools sort - -f %s/%s_lookback.bam" &'%tuple([d00d[0]]*5))
-	os.system('bsub -J retromap_%s -w "done(DBlookback_%s)" -o retromap_%s.lsf.out "samtools view -F4 %s/%s_lookback.bam > %s/%s_lookback_found.sam" &'%tuple([d00d[0]]*7))
+	os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([d00d[0]]*5))
+	os.system('bsub -J retromap_%s -w "done(DBlookback_%s)" -o retromap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([d00d[0]]*7))
 os.system('while [[ `bjobs -w | grep retromap_ | wc -l` -gt 0 ]]; do sleep 60; bar=$(); f="#"; percent=$((100*$(bjobs -w | grep retromap_ | wc -l)/%s)) ; for (( c=1; c<=$((100-$percent)); c++)); do bar=$bar$f; done; echo -ne "$percent%% remaining $bar\r" ; done; echo "\n" '%tuple([len(all_peeps)]) )
 
 
 ###	3	:	play hide and seq
 
+retro_dict={}
+for d00d in all_peeps:
+	ret_list =np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([d00d[0]]*2), shell=True).split('\n'))
+	retro_dict[d00d[1]] = list(ret_list[ret_list!=''])
+
+for fund in all_finds:
+	retro_dict[fund[1]].pop(retro_dict[fund[1]].index(fund[2]))
+
+for key in retro_dict.keys():
+	print "%s had was retroactively detected in %s individuals.\n"%tuple([key, len(retro_dict[key])])
 
 
-
-
+###	4	:	add a flag field to the DB?
 
 
 curr.close()
