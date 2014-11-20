44cc2fac832528c89739b8a4464f19fb7410dbce
csoeder <charles.soeder@gmail.com>
2014-11-20 13:35:32 -0500

    grep correctly dammit

--- pipeline_v3.5.sh
+++ pipeline_v3.5.sh
@@ -39,7 +39,7 @@ module load bowtie
 echo "$(date)	META:		modules loaded"	>> monitor.log
 #############################################
 if [[ ! -f Trinity_files.Trinity.fasta || ! -f $1_mapsplice_alignment.sam ]]; then
-	for fastq in `grep $s ../dwnld_list.txt | cut -f 29`; do wget $fastq; gzip -d $fastq; done
+	for fastq in `grep $1 ../dwnld_list.txt | cut -f 29`; do wget $fastq; gzip -d $fastq; done
 echo "RNA-Seq reads:	" >> monitor.log	#	Note the lack of Trinity assembly frontload
 du ERR* >> monitor.log								#
 fi
