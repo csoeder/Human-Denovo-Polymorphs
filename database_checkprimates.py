0686dd99d5d2fdd7399ee6412f22141194740d43
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-03 11:25:08 -0500

    /lustre isn't available on biodev; relocated the primate transcriptome files

--- database_checkprimates.py
+++ database_checkprimates.py
@@ -8,13 +8,13 @@ import csv
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import pysam
-from subprocess import call, check_output
+from subprocess import call, check
 
 pwd = sys.argv[1]	#password
 conn = psycopg2.connect("dbname=denovogenes user=gene password=%s host=bioapps.its.unc.edu"%pwd)
 curr = conn.cursor()
 
-primate_omes = '/lustre/scr/c/s/csoeder/primates/'
+primate_omes = '/netscr/csoeder/1kGen/data/primateomes/'
 
 
 missing_dict = {}	#	Were any sequences missing when the transcriptomes were reexamined?
@@ -48,7 +48,7 @@ phial.close()
 ###	2	:	align it to ALL the transcriptomes
 
 
-for sample in check_output('ls %s | grep -v the'%primate_omes, shell=True).split('\n'):
+for sample in check_output('ls %s | grep -v the | grep -v out'%primate_omes, shell=True).split('\n'):
 	os.system('bsub -J primalign_%s -o primalign_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([sample,sample, '%s/%s'%tuple([primate_omes,sample]), '%s/%s'%tuple([primate_omes,sample]), sample]))
 	os.system('bsub -J primmap_%s -w "done(primalign_%s)" -o primmap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([sample, sample, sample, '%s/%s'%tuple([primate_omes,sample]), sample, '%s/%s'%tuple([primate_omes,sample]), sample]))
 os.system('while [[ `bjobs -w | grep primmap_ | wc -l` -gt 0 ]]; do sleep 60; bar=$(); f="#"; percent=$((100*$(bjobs -w | grep retromap_ | wc -l)/%s)) ; for (( c=1; c<=$((100-$percent)); c++)); do bar=$bar$f; done; echo -ne "$percent%% remaining $bar\r" ; done; echo "\n" '%tuple([4]) )
@@ -57,9 +57,9 @@ os.system('while [[ `bjobs -w | grep primmap_ | wc -l` -gt 0 ]]; do sleep 60; ba
 ###	3	:	play hide and seq
 
 
-for sample in check_output('ls %s | grep -v the | grep pan'%primate_omes, shell=True).split('\n'):
+for sample in check_output('ls %s | grep -v the | grep -v out | grep pan'%primate_omes, shell=True).split('\n'):
 	chumps = set(np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
-for sample in check_output('ls %s | grep -v the | grep gor'%primate_omes, shell=True).split('\n'):
+for sample in check_output('ls %s | grep -v the | grep -v out | grep gor'%primate_omes, shell=True).split('\n'):
 	gorillaz = set(np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
 
 print "%s candidates observed in the transcriptomes of both chimp and gorilla: %s\n"%tuple([len(chumps.intersection(gorillaz)), '%s, '*len(chumps.intersection(gorillaz))%tuple(chumps.intersection(gorillaz))])
