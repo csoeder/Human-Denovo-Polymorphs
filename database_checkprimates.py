71bd8adb9085052c79da78d719b6afc3e44018bd
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-03 11:56:05 -0500

    filter out null results; maybe fixed ls

--- database_checkprimates.py
+++ database_checkprimates.py
@@ -51,14 +51,16 @@ phial.close()
 
 
 for sample in check_output('ls %s | grep -v the | grep -v out'%primate_omes, shell=True).split('\n'):
-	print sample
-	os.system('bsub -J primalign_%s -o primalign_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([sample,sample, '%s/%s'%tuple([primate_omes,sample]), '%s/%s'%tuple([primate_omes,sample]), sample]))
-	sleep(1)
+	if sample !=' '
+		print sample
+		os.system('bsub -J primalign_%s -o primalign_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([sample,sample, '%s/%s'%tuple([primate_omes,sample]), '%s/%s'%tuple([primate_omes,sample]), sample]))
+		sleep(1)
 
 for sample in check_output('ls %s | grep -v the | grep -v out'%primate_omes, shell=True).split('\n'):
-	print sample
-	os.system('bsub -J primmap_%s -w "done(primalign_%s)" -o primmap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([sample, sample, sample, '%s/%s'%tuple([primate_omes,sample]), sample, '%s/%s'%tuple([primate_omes,sample]), sample]))
-	sleep(1)
+	if sample !=' '
+		print sample
+		os.system('bsub -J primmap_%s -w "done(primalign_%s)" -o primmap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([sample, sample, sample, '%s/%s'%tuple([primate_omes,sample]), sample, '%s/%s'%tuple([primate_omes,sample]), sample]))
+		sleep(1)
 
 os.system('while [[ `bjobs -w | grep primmap_ | wc -l` -gt 0 ]]; do sleep 60; bar=$(); f="#"; percent=$((100*$(bjobs -w | grep retromap_ | wc -l)/%s)) ; for (( c=1; c<=$((100-$percent)); c++)); do bar=$bar$f; done; echo -ne "$percent%% remaining $bar\r" ; done; echo "\n" '%tuple([4]) )
 
@@ -67,9 +69,9 @@ os.system('while [[ `bjobs -w | grep primmap_ | wc -l` -gt 0 ]]; do sleep 60; ba
 
 
 for sample in check_output('ls %s | grep -v the | grep -v out | grep pan'%primate_omes, shell=True).split('\n'):
-	chumps = set(np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
+	chumps = set(np.array(check_output('cut -f 4 %s%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
 for sample in check_output('ls %s | grep -v the | grep -v out | grep gor'%primate_omes, shell=True).split('\n'):
-	gorillaz = set(np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
+	gorillaz = set(np.array(check_output('cut -f 4 %s%s_lookback.bed | sort | uniq'%tuple([primate_omes, sample]), shell=True).split('\n')))
 
 print "%s candidates observed in the transcriptomes of both chimp and gorilla: %s\n"%tuple([len(chumps.intersection(gorillaz)), '%s, '*len(chumps.intersection(gorillaz))%tuple(chumps.intersection(gorillaz))])
 print "%s candidates were observed in chimps only: %s\n"%tuple([len(chumps.difference(gorillaz)), '%s, '*len(chumps.difference(gorillaz))%tuple(chumps.difference(gorillaz))])
