b20f46a33d280edf025e6d14e2ae27f07e02a575
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-03 11:30:31 -0500

    fixed bsub loops so that LSF doesn't trip over its own shoelaces

--- database_checkprimates.py
+++ database_checkprimates.py
@@ -49,7 +49,10 @@ phial.close()
 
 
 for sample in check_output('ls %s | grep -v the | grep -v out'%primate_omes, shell=True).split('\n'):
+	echo $sample
 	os.system('bsub -J primalign_%s -o primalign_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([sample,sample, '%s/%s'%tuple([primate_omes,sample]), '%s/%s'%tuple([primate_omes,sample]), sample]))
+for sample in check_output('ls %s | grep -v the | grep -v out'%primate_omes, shell=True).split('\n'):
+	echo $sample
 	os.system('bsub -J primmap_%s -w "done(primalign_%s)" -o primmap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([sample, sample, sample, '%s/%s'%tuple([primate_omes,sample]), sample, '%s/%s'%tuple([primate_omes,sample]), sample]))
 os.system('while [[ `bjobs -w | grep primmap_ | wc -l` -gt 0 ]]; do sleep 60; bar=$(); f="#"; percent=$((100*$(bjobs -w | grep retromap_ | wc -l)/%s)) ; for (( c=1; c<=$((100-$percent)); c++)); do bar=$bar$f; done; echo -ne "$percent%% remaining $bar\r" ; done; echo "\n" '%tuple([4]) )
 
