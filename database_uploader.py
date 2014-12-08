397f1d2bf66b5d69343281568366b0ad65ecc746
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-08 11:13:35 -0500

    check for empty exon files

--- database_uploader.py
+++ database_uploader.py
@@ -34,45 +34,49 @@ for rho in dats:
 		person_pk = curr.fetchone()[0]
 		#print deNovos
 		for exon in deNovos:
-#			dats = csv.reader( open('%s/mapt/chunked_genes/deNovos/%s'%tuple([ide, exon]),'r'), delimiter='\t').next()
-			print exon
-			dats = csv.reader( open('/nas02/home/c/s/csoeder/Denovo_Candidates/%s/%s'%tuple([ide, exon]),'r'), delimiter='\t').next()
-			chro, begin, end, script_tag = dats[:4]
-			seq_query = check_output(['samtools', 'faidx', '%s/Trinity_files.Trinity.fasta'%ide, script_tag])
-			transcript_seq = ''.join(seq_query.split('\n')[1:]).upper()#			the sequence which appears in the transcriptome
-			seq_query = check_output(['samtools', 'faidx', '../data/hg19.fa', '%s:%s-%s'%tuple([chro, begin, end])])
-			reference_seq = ''.join(seq_query.split('\n')[1:]).upper()#				the sequence which appears in the reference genome
+			try:
+	#			dats = csv.reader( open('%s/mapt/chunked_genes/deNovos/%s'%tuple([ide, exon]),'r'), delimiter='\t').next()
+				print exon
 
-			try:#is the location already entered in the database, ie, this site has been detected as expressed?
-				curr.execute("SELECT id FROM location WHERE chrom = %s AND start = %s AND stop = %s AND ref = 'hg19';", tuple([chro, begin, end]))
-				loc_pk = curr.fetchone()[0]
-			except TypeError:#if not, put it into the appropriate table
-				curr.execute('INSERT INTO location (chrom, start, stop, ref, poly) VALUES (%s, %s, %s, %s, %s);', tuple([chro, begin, end, 'hg19', True]))
-				curr.execute("SELECT id FROM location WHERE chrom = %s AND start = %s AND stop = %s AND ref = 'hg19';", tuple([chro, begin, end]))
-				loc_pk = curr.fetchone()[0]
+				dats = csv.reader( open('/nas02/home/c/s/csoeder/Denovo_Candidates/%s/%s'%tuple([ide, exon]),'r'), delimiter='\t').next()
+				chro, begin, end, script_tag = dats[:4]
+				seq_query = check_output(['samtools', 'faidx', '%s/Trinity_files.Trinity.fasta'%ide, script_tag])
+				transcript_seq = ''.join(seq_query.split('\n')[1:]).upper()#			the sequence which appears in the transcriptome
+				seq_query = check_output(['samtools', 'faidx', '../data/hg19.fa', '%s:%s-%s'%tuple([chro, begin, end])])
+				reference_seq = ''.join(seq_query.split('\n')[1:]).upper()#				the sequence which appears in the reference genome
 
-			try:#is the transcriptome sequence one which has already been identified? 
-				curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple(transcript_seq))
-				trans_seq_pk = curr.fetchone()[0]
-			except TypeError:#if not, put it in the DB
-				curr.execute("INSERT INTO sequence (seq, ref) VALUES (%s, %s);", tuple([transcript_seq, 'hg19']))
-				curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple([transcript_seq]))
-				trans_seq_pk = curr.fetchone()[0]
+				try:#is the location already entered in the database, ie, this site has been detected as expressed?
+					curr.execute("SELECT id FROM location WHERE chrom = %s AND start = %s AND stop = %s AND ref = 'hg19';", tuple([chro, begin, end]))
+					loc_pk = curr.fetchone()[0]
+				except TypeError:#if not, put it into the appropriate table
+					curr.execute('INSERT INTO location (chrom, start, stop, ref, poly) VALUES (%s, %s, %s, %s, %s);', tuple([chro, begin, end, 'hg19', True]))
+					curr.execute("SELECT id FROM location WHERE chrom = %s AND start = %s AND stop = %s AND ref = 'hg19';", tuple([chro, begin, end]))
+					loc_pk = curr.fetchone()[0]
+
+				try:#is the transcriptome sequence one which has already been identified? 
+					curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple(transcript_seq))
+					trans_seq_pk = curr.fetchone()[0]
+				except TypeError:#if not, put it in the DB
+					curr.execute("INSERT INTO sequence (seq, ref) VALUES (%s, %s);", tuple([transcript_seq, 'hg19']))
+					curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple([transcript_seq]))
+					trans_seq_pk = curr.fetchone()[0]
 
 
-			try:#is the reference sequence one which has already been identified? 
-				curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple(reference_seq))
-				ref_seq_pk = curr.fetchone()[0]
-			except TypeError:#if not, put it in the DB
-				curr.execute("INSERT INTO sequence (seq, ref) VALUES (%s, %s);", tuple([reference_seq, 'hg19']))
-				curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple([reference_seq]))
-				ref_seq_pk = curr.fetchone()[0]
+				try:#is the reference sequence one which has already been identified? 
+					curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple(reference_seq))
+					ref_seq_pk = curr.fetchone()[0]
+				except TypeError:#if not, put it in the DB
+					curr.execute("INSERT INTO sequence (seq, ref) VALUES (%s, %s);", tuple([reference_seq, 'hg19']))
+					curr.execute("SELECT id FROM sequence WHERE seq = %s AND ref = 'hg19';", tuple([reference_seq]))
+					ref_seq_pk = curr.fetchone()[0]
 
-			try:#has this piece of data already been recorded? No idea why it would be, but I'm trying hard here
-				curr.execute("SELECT id FROM find WHERE source=%s AND seq=%s AND loc=%s;", tuple([person_pk, trans_seq_pk, loc_pk]))
-				datum = curr.fetchone()[0]
-			except TypeError:#mostly its going to just add data to th DB
-				curr.execute("INSERT INTO find (source, seq, loc) VALUES (%s, %s, %s);", tuple([person_pk, trans_seq_pk, loc_pk]) )
+				try:#has this piece of data already been recorded? No idea why it would be, but I'm trying hard here
+					curr.execute("SELECT id FROM find WHERE source=%s AND seq=%s AND loc=%s;", tuple([person_pk, trans_seq_pk, loc_pk]))
+					datum = curr.fetchone()[0]
+				except TypeError:#mostly its going to just add data to th DB
+					curr.execute("INSERT INTO find (source, seq, loc) VALUES (%s, %s, %s);", tuple([person_pk, trans_seq_pk, loc_pk]) )
+			except StopIteration:
+				print "%s is empty"%tuple([exon]) 
 
 		#de_novs = os.listdir('%s/mapt/chunked_genes/deNovos/'%ide)
 		print "%s dumped to database" % tuple([ide])
