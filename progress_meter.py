0099f916c29b9283fbd40fe96425eeee02db47c9
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-18 12:29:25 -0500

    improved progress graphing

--- progress_meter.py
+++ progress_meter.py
@@ -9,6 +9,7 @@ from subprocess import call, check_output
 
 mer_de_noms = check_output("bjobs -w | grep grind | cut -f 3 -d _ | cut -f 1 -d ' ' | sort | uniq", shell=True).split('\n')[:-1]
 
+mucks = 0
 for name in mer_de_noms:
 	dats = check_output("for jorb in $(bjobs -w | grep RUN | grep %s | grep grind | cut -f 1 -d ' '); do c=$(bpeek $jorb | grep -c ccumulated); echo $c ; done"%tuple([name]), shell=True)
 
@@ -16,10 +17,11 @@ for name in mer_de_noms:
 	for d in dats.split('\n')[:-1]:
 		cleandats.append(float(d)/2)
 	if len(cleandats) > 0:
-		plt.hist(cleandats, bins=25, histtype='step')
+		n, bins, patches = plt.hist(cleandats, bins=30, histtype='step', color='b')
+		mucks = max(mucks, max(n))
 
 
-
+plt.ylim(0, 1.5*mucks)
 plt.show()
 plt.savefig("Progress_Histogram.png")
 
