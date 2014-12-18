28f19be7d9d01dd01ef6e89fc7b75a85d5574014
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-18 10:55:53 -0500

    a histrogram for tracking align-grind progress

---
+++ progress_meter.py
@@ -0,0 +1,30 @@
+import csv
+import sys
+import numpy as np
+import matplotlib 
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
+from Bio import SeqIO
+from subprocess import call, check_output
+
+mer_de_noms = check_output("bjobs -w | grep grind | cut -f 3 -d _ | cut -f 1 -d ' ' | sort | uniq", shell=True).split('\n')[:-1]
+
+for name in mer_de_noms:
+	dats = check_output("for jorb in $(bjobs -w | grep RUN | grep %s | grep grind | cut -f 1 -d ' '); do c=$(bpeek $jorb | grep -c ccumulated); echo $c ; done"%tuple([name]), shell=True)
+
+	cleandats =[]
+	for d in dats.split('\n')[:-1]:
+		cleandats.append(float(d)/2)
+
+	plt.hist(cleandats, bins=25, histtype='step')
+
+
+
+plt.show()
+plt.savefig("Progress_Histogram.png")
+
+
+
+
+
+
