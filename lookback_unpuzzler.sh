7293a933c2a4606e8a0e81fe908e6a7081464414
csoeder <charles.soeder@gmail.com>
2014-12-28 19:45:10 -0500

    unpuzzler script written to interpret lookback results

---
+++ lookback_unpuzzler.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SAMPLE_SIZE=$1
+WINDOW_SIZE=1000
+
+for i in {1..$SAMPLE_SIZE};	do
+
+	problem=$(head -$((${RANDOM} % `wc -l < lookback_report.dat`+1 )) lookback_report.dat | tail -1);
+
+	location=$(cut -f 1 lookback_report.dat);
+	tagged_carriers=$(cut -f 2 lookback_report.dat);
+	silent_carriers=$(cut -f 3 lookback_report.dat);
+
+	echo $location;
+	chro=$(echo $location | cut -f 1 -d :);
+	start=$(echo $location | cut -f 2 -d : | cut -f 1 -d -);
+	stop=$(echo $location | cut -f 2 -d : | cut -f 2 -d -);
+
+	new_start=$(($((start-WINDOW_SIZE))>1?$((start-WINDOW_SIZE)):1))	#take the max of the windowed min and 1 to avoid negative coords
+	new_stop=$((stop+WINDOW_SIZE))	#	No biggie for stops - samtools/UCSC won't freak
+	new_location=$chro:$new_start-$new_stop
+	tagged=$(echo $tagged_carriers | cut -f $((${RANDOM} % `echo $tagged_carriers | tr ',' '\n' | wc -l `+1 )) -d ',' );	#choose a random tagged expresser
+	silent=$(echo $silent_carriers | cut -f $((${RANDOM} % `echo $silent_carriers | tr ',' '\n' | wc -l `+1 )) -d ',' );	#choose a random silent expresser
+	
+	samtools view -hb $tagged/$tagged_mapsplice_alignment.sort.bam $new_location | bedtools bamtobed -bed12 -color 0,0,255 -i - >> unpuzzler_tagged.bed;
+	samtools view -hb $silent/$silent_mapsplice_alignment.sort.bam $new_location | bedtools bamtobed -bed12 -color 255,0,0 -i - >> unpuzzler_silent.bed;
+	done;
+
