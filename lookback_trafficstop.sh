f44114c57f482949516c0f490fc3dbd4c43437ca
Charles Soeder <Charles.Soeder@gmail.com>
2015-01-05 14:21:41 -0500

    fixt variable stringing-togetherness

--- lookback_trafficstop.sh
+++ lookback_trafficstop.sh
@@ -13,8 +13,8 @@ do
 
 	for person in $(tail -n +2 dwnld_list.txt| cut -f 1 | sort | uniq );
 		do echo $person;
-		transcript=$(grep -P "^[a-zA-Z0-9_]*\t[0-9]*\t[0-9]*\t$a\t" $person/$person_lookback.bed | cut -f 1);
-		grep $transcript $person_Assemblies_mapped.bed >> review.bed;
+		transcript=$(grep -P "^[a-zA-Z0-9_]*\t[0-9]*\t[0-9]*\t$a\t" "$person"/"$person"_lookback.bed | cut -f 1);
+		grep $transcript "person"/"$person"_Assemblies_mapped.bed >> review.bed;
 	done;
 
 	bedtools intersect -split -a review.bed -b $DATA_DIR/repeatmasker.bed > no_rpts.bed;				#
