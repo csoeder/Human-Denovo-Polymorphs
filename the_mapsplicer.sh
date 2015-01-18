07dab70a8d6466d8a4c88b7e50a15588c6944aa9
csoeder <charles.soeder@gmail.com>
2015-01-18 16:53:59 -0500

    bsub commands cleaned up?

--- the_mapsplicer.sh
+++ the_mapsplicer.sh
@@ -4,14 +4,15 @@
 source ../pipeline_config.sh	#################################
 FOLDER=$1
 ###Mapsplice the reads#######################################
-if [ ! -f $FOLDER_mapsplice_alignment.sam ]; then
+if [[ ! -f "$FOLDER"_mapsplice_alignment.sam ]]; then
 #	bsub -J mapsplut_$FOLDER -o mapsplut.lsf.out -M 75 -q week python /nas02/apps/mapsplice-2.1.4/src/MapSplice-v2.1.4/mapsplice.py -c /netscr/csoeder/1kGen/data/hg19_split/ -x /netscr/csoeder/1kGen/data/hg19_bowtieindex/hg19 -1 ERR*_1.fastq -2 ERR*_2.fastq
-	bsub -J mapsplut_$FOLDER -o mapsplut.lsf.out -q week python /nas02/apps/mapsplice-2.1.4/src/MapSplice-v2.1.4/mapsplice.py -c /netscr/csoeder/1kGen/data/hg19_split/ -x /netscr/csoeder/1kGen/data/hg19_bowtieindex/hg19 -1 ERR*_1.fastq -2 ERR*_2.fastq
-	bsub -J mvsplut_$FOLDER -w "done(mapsplut_$FOLDER)" -o mvsplut.lsf.out  mv mapsplice_out/alignments.sam $FOLDER_mapsplice_alignment.sam
-	bsub -J splutvert_$FOLDER -w "done(mvsplut_$FOLDER)" -o splutvert.lsf.out "samtools view -Sbh $FOLDER_mapsplice_alignment.sam > $FOLDER_mapsplice_alignment.bam"
-	bsub -J bamsort_$FOLDER -w "done(splutvert_$FOLDER)" -o bamsort.lsf.out samtools sort $FOLDER_mapsplice_alignment.bam $FOLDER_mapsplice_alignment.sort
-	bsub -J splutDex_$FOLDER -w "done(bamsort_$FOLDER)" -o splutDex.lsf.out samtools index $FOLDER_mapsplice_alignment.sort.bam
-	bsub -J bam2bed_$FOLDER -w "done(bamsort_$FOLDER)" -o bam2bed.lsf.out "bedtools bamtobed -bed12 -i $FOLDER_mapsplice_alignment.sort.bam > $FOLDER_mapsplice_alignment.bed"
+$	bsub -J mapsplut_$FOLDER -o mapsplut.lsf.out -q week python /nas02/apps/mapsplice-2.1.4/src/MapSplice-v2.1.4/mapsplice.py -c /netscr/csoeder/1kGen/data/hg19_split/ -x /netscr/csoeder/1kGen/data/hg19_bowtieindex/hg19 -1 ERR*_1.fastq -2 ERR*_2.fastq
+	bsub -J mapsplut_$FOLDER -o mapsplut.lsf.out -q week sleep 10
+	bsub -J mvsplut_$FOLDER -w "done(mapsplut_$FOLDER)" -o mvsplut.lsf.out  cp mapsplice_out/alignments.sam "$FOLDER"_mapsplice_alignment.sam
+	bsub -J splutvert_$FOLDER -w "done(mvsplut_$FOLDER)" -o splutvert.lsf.out "samtools view -Sbh "$FOLDER"_mapsplice_alignment.sam > "$FOLDER"_mapsplice_alignment.bam"
+	bsub -J bamsort_$FOLDER -w "done(splutvert_$FOLDER)" -o bamsort.lsf.out samtools sort "$FOLDER"_mapsplice_alignment.bam "$FOLDER"_mapsplice_alignment.sort
+	bsub -J splutDex_$FOLDER -w "done(bamsort_$FOLDER)" -o splutDex.lsf.out samtools index "$FOLDER"_mapsplice_alignment.sort.bam
+	bsub -J bam2bed_$FOLDER -w "done(bamsort_$FOLDER)" -o bam2bed.lsf.out "bedtools bamtobed -bed12 -i "$FOLDER"_mapsplice_alignment.sort.bam > "$FOLDER"_mapsplice_alignment.bed"
 	echo "$(date)	MAPSPLICER:		MapSplice jobs submitted"	>> monitor.log
 else
 	bsub -J splutDex_$FOLDER -o splutDex.lsf.out echo "trivial"
