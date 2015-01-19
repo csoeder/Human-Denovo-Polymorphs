eb1880d0f993a6c195ce9c350b4d5671aead36f0
Charles Soeder <Charles.Soeder@gmail.com>
2015-01-19 10:25:47 -0500

    take this code to the quotationg station

--- post_mapsplice.sh
+++ post_mapsplice.sh
@@ -16,9 +16,9 @@ bsub -J faidx_$FOLDER -w "done(bwaInducks_$FOLDER)" -o faidx.lsf.out samtools fa
 #############################################################################
 ### Align the Trinity #######################################
 # bsub -J TrinAlign_$FOLDER -M 50 -w "done(faidx_$FOLDER)" -o TrinAlign.lsf.out bowtie2 -f -x $DATA_DIR/hg19_bowtieindex/hg19 -U Trinity_files.Trinity.fasta -S $FOLDER_aligned.sam
-bsub -J TrinAlign_$FOLDER -w "done(faidx_$FOLDER)" -o TrinAlign.lsf.out bowtie2 -f -x $DATA_DIR/hg19_bowtieindex/hg19 -U Trinity_files.Trinity.fasta -S $FOLDER_aligned.sam
-bsub -J bamvert2_$FOLDER -w "done(TrinAlign_$FOLDER)" -o bamvert2.lsf.out "samtools view -bS $FOLDER_aligned.sam > $FOLDER_Assembly_Alignment.bam"
-bsub -J bamsort2_$FOLDER -w "done(bamvert2_$FOLDER)" -o bamsort2.lsf.out "samtools sort $FOLDER_Assembly_Alignment.bam $FOLDER_Assembly_Alignment.sorted"
+bsub -J TrinAlign_$FOLDER -w "done(faidx_$FOLDER)" -o TrinAlign.lsf.out bowtie2 -f -x $DATA_DIR/hg19_bowtieindex/hg19 -U Trinity_files.Trinity.fasta -S "$FOLDER"_aligned.sam
+bsub -J bamvert2_$FOLDER -w "done(TrinAlign_$FOLDER)" -o bamvert2.lsf.out "samtools view -bS "$FOLDER"_aligned.sam > "$FOLDER"_Assembly_Alignment.bam"
+bsub -J bamsort2_$FOLDER -w "done(bamvert2_$FOLDER)" -o bamsort2.lsf.out "samtools sort "$FOLDER"_Assembly_Alignment.bam "$FOLDER"_Assembly_Alignment.sorted"
 #############################################################################
 ###	Align the paired-end reads to the Trinity transcripts;	#						As long as the unmapped fork is disabled, these jobs are a 
 ###	hopefully rescue unaligned assemblies in the unaligned 	#						waste of gigaflops & gigabytes
@@ -32,20 +32,20 @@ bsub -J bamsort2_$FOLDER -w "done(bamvert2_$FOLDER)" -o bamsort2.lsf.out "samtoo
 #############################################################################
 ### Partition the Transcripts into those which align and # 
 ### those which do not. ##################################
-bsub -J gather_maps_$FOLDER -w "done(bamsort2_$FOLDER)" -o gather_maps.lsf.out "samtools view -h -F4 $FOLDER_Assembly_Alignment.sorted.bam > $FOLDER_Assemblies_mapped.sam"
-bsub -J sort_maps_$FOLDER -w "done(bamsort2_$FOLDER)" -o gather_maps.lsf.out "samtools view -hb -F4 $FOLDER_Assembly_Alignment.sorted.bam | samtools sort - $FOLDER_Assemblies_mapped.sort "
-bsub -J bed_maps_$FOLDER -w "done(sort_maps_$FOLDER)" -o gather_maps.lsf.out "bedtools bamtobed -bed12 -i $FOLDER_Assemblies_mapped.sort.bam > $FOLDER_Assemblies_mapped.bed"
+bsub -J gather_maps_$FOLDER -w "done(bamsort2_$FOLDER)" -o gather_maps.lsf.out "samtools view -h -F4 "$FOLDER"_Assembly_Alignment.sorted.bam > "$FOLDER"_Assemblies_mapped.sam"
+bsub -J sort_maps_$FOLDER -w "done(bamsort2_$FOLDER)" -o gather_maps.lsf.out "samtools view -hb -F4 "$FOLDER"_Assembly_Alignment.sorted.bam | samtools sort - "$FOLDER"_Assemblies_mapped.sort "
+bsub -J bed_maps_$FOLDER -w "done(sort_maps_$FOLDER)" -o gather_maps.lsf.out "bedtools bamtobed -bed12 -i "$FOLDER"_Assemblies_mapped.sort.bam > "$FOLDER"_Assemblies_mapped.bed"
 
 #bsub -J gather_unmaps_$1 -w "done(bamsort2_$1)" -o gather_unmaps.lsf.out "samtools view -h -f4 $1_Assembly_Alignment.sorted.bam > $1_Assemblies_unmapped.sam"
 #	ignore unaligned fork
 #############################################################################
 ### Fork a job on each partition ############################
-bsub -J $FOLDER_aligned_fork -q week -w "done(faidx_$FOLDER) && done(gather_maps_$FOLDER)" -q week -o aligned_fork.lsf.out "sh $SCRIPT_DIR/aligned_fork_v3.5_parallel.sh $FOLDER_Assemblies_mapped.sam $FOLDER_mapsplice_alignment.sort.bam Trinity_files.Trinity.fasta $FOLDER"
+bsub -J "$FOLDER"_aligned_fork -q week -w "done(faidx_$FOLDER) && done(gather_maps_$FOLDER)" -q week -o aligned_fork.lsf.out "sh $SCRIPT_DIR/aligned_fork_v3.5_parallel.sh "$FOLDER"_Assemblies_mapped.sam "$FOLDER"_mapsplice_alignment.sort.bam Trinity_files.Trinity.fasta $FOLDER"
 #bsub -J $1_unaligned_fork -q week -w "done(faidx_$1) && done(gather_unmaps_$1) && done(rnaInducks_$1)" -o unaligned_fork.lsf.out -q week "sh $SCRIPT_DIR/unaligned_fork_v3.5_parallel.sh $1_Assemblies_unmapped.sam $1_mapsplice_alignment.sort.bam Trinity_files.Trinity.fasta RNASeq_vs_Trinity.sort.bam $1_mapsplice_alignment.bed $1"
 #	ignore the unaligned fork
 ############################################################################
 ### Is it done? Email me. ###################################
-bsub -J $FOLDER_alert -w "done($FOLDER_aligned_fork)" echo $FOLDER has successfully completed
+bsub -J "$FOLDER"_alert -w "done("$FOLDER"_aligned_fork)" echo $FOLDER has successfully completed
 #############################################################################
 
 
