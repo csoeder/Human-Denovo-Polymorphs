7c4596722952de47347575748b8319cb22c40461
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-02 15:04:11 -0500

    config file updated

--- pipeline_config.sh
+++ pipeline_config.sh
@@ -11,8 +11,8 @@ SUBMISSION_DELAY=1200	# 1200 seconds between submits ~ 3 per hour ~ 462 per week
 ###############################################################
 # bundle parameters for the accumulator #######################
 #	see aligned_fork ##########################################
-BATCH_SIZE=75 #There are 200 datumz processed per bundle
-WAIT_TIME=60 #	Wait 90 seconds before submitting next bundle
+BATCH_SIZE=300 #There are 300 datumz processed per bundle
+WAIT_TIME=30 #	Wait 30 seconds before submitting next bundle
 COUNTER=0 #		Nothing in the present bundle
 ###############################################################
 
