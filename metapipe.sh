76893829c0777dac008e672d0b092608ebc19ed6
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-11 23:08:17 -0500

    typo

--- metapipe.sh
+++ metapipe.sh
@@ -9,7 +9,7 @@ filter=$1	#	to select a subset of individuals - usually HG or NA
 
 echo "w00tw00t we're piping" | write csoeder
 
-for folder in $( cut -f 1 $SCRIPT_DIR/dwnld_list.txt | grep -v Source | grep $filter | sort | uniq`; do
+for folder in $( cut -f 1 $SCRIPT_DIR/dwnld_list.txt | grep -v Source | grep $filter | sort | uniq); do
 	echo "$folder queued" | write csoeder;
 	sh  $SCRIPT_DIR/pipeline.sh $folder > redirect_$folder.out;
 	echo "$folder submitted" | write csoeder;
