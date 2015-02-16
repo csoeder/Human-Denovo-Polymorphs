1b5f1b7fd421563688033205d9bcdd05e41360d0
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-16 15:03:48 -0500

    database_lookback rewritten with paths

--- database_lookback.py
+++ database_lookback.py
@@ -13,6 +13,10 @@ from subprocess import call, check_output
 from time import sleep
 import pickle
 
+script_path='/netscr/csoeder/1kGen/query-process/'
+working_path='/netscr/csoeder/1kGen/individuals/'
+data_path='/netscr/csoeder/1kGen/data/'
+
 pwd = sys.argv[1]	#password
 conn = psycopg2.connect("dbname=denovogenes user=gene password=%s host=bioapps.its.unc.edu"%pwd)
 curr = conn.cursor()
@@ -32,13 +36,10 @@ curr.execute("SELECT id, pk FROM person;")
 all_peeps = curr.fetchall()
 
 
-
-
-
 ###	1	:	Grep the sequence
 
 phial = open('lookback.fasta', 'w')
-for find in all_finds:
+for find in all_finds:	#	For each sighting, record who expresses each location
 	if find[2] in start_dict.keys():
 		start_dict[find[2]].append(find[1])
 	else:
@@ -53,56 +54,54 @@ phial.close()
 
 for d00d in all_peeps:
 #	os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh | samtools sort - -f %s/%s_lookback.bam" &'%tuple([d00d[0]]*5))
-	os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s/%s_lookback.bam" &'%tuple([d00d[0]]*5))
-	sleep(0.1)
+	if os.path.isfile('%s%s/Trinity_files.Trinity.fasta'%tuple([working_path,d00d])):
+		os.system('bsub -J DBlookback_%s -o DBlookback_%s.lsf.out "bwa mem %s%s/Trinity_files.Trinity.fasta lookback.fasta | samtools view -Sbh - | samtools sort - -f %s%s/%s_lookback.bam" &'%tuple([d00d[0],d00d[0],working_path,d00d[0],working_path,d00d[0],d00d[0]]))
+		sleep(0.1)
 sleep(0.5)
 for d00d in all_peeps:
-	os.system('bsub -J retromap_%s -w "done(DBlookback_%s)" -o retromap_%s.lsf.out "samtools view -b -F4 %s/%s_lookback.bam | bedtools bamtobed -i - > %s/%s_lookback.bed " &'%tuple([d00d[0]]*7))
-	sleep(0.1)
+	if os.path.isfile('%s%s/Trinity_files.Trinity.fasta'%tuple([working_path,d00d])):
+		os.system('bsub -J retromap_%s -w "done(DBlookback_%s)" -o retromap_%s.lsf.out "samtools view -b -F4 %s%s/%s_lookback.bam | bedtools bamtobed -i - > %s%s/%s_lookback.bed " &'%tuple([d00d[0],d00d[0],d00d[0],working_path,d00d[0],d00d[0],working_path,d00d[0],d00d[0],]))
+		sleep(0.1)
 os.system('while [[ `bjobs -w | grep retromap_ | wc -l` -gt 0 ]]; do sleep 60; bar=$(); f="#"; percent=$((100*$(bjobs -w | grep retromap_ | wc -l)/%s)) ; for (( c=1; c<=$((100-$percent)); c++)); do bar=$bar$f; done; echo -ne "$percent%% remaining $bar\r" ; done; echo "\n" '%tuple([len(all_peeps)]) )
 
+#	
 
 ###	3	:	play hide and seq
 
-retro_dict={}
+retro_dict={}			#	This is dict has identifiers as keys, values are lists of sites seen expressed on lookback
 for d00d in all_peeps:
-	#ret_list =np.array(check_output('cut -f 4 %s/%s_lookback.bed | sort | uniq'%tuple([d00d[0]]*2), shell=True).split('\n'))
-	ret_list =np.array(check_output('grep + %s/%s_lookback.bed | cut -f 4 | sort | uniq'%tuple([d00d[0]]*2), shell=True).split('\n'))
+	ret_list =np.array(check_output('grep + %s%s/%s_lookback.bed | cut -f 4 | sort | uniq'%tuple([working_path,d00d[0],d00d[0]]), shell=True).split('\n'))
 	retro_dict[d00d[1]] = list(ret_list[ret_list!=''])
 
-
-#print retro_dict
-old_dict = {}
+old_dict = {}	#	Make a copy for later use
 for i in retro_dict.keys():
 	old_dict[i] = retro_dict[i]
 
-for fund in all_finds:
+for fund in all_finds:	#	If a site found on lookback is recorded in the DB, it's not a problem - remove from the list
 	retro_dict[fund[1]].pop(retro_dict[fund[1]].index(str(fund[2])))
 
-
-new_dict = {}
+new_dict = {}	#	Another copy for comparison
 for i in retro_dict.keys():
 	new_dict[i] = len(retro_dict[i])
 
 
-for key in retro_dict.keys():
+#for key in retro_dict.keys():
 	#print "%s had was retroactively detected in %s individuals.\n"%tuple([key, len(retro_dict[key])])
 	#print key, old_dict[key], new_dict[key], retro_dict[key][:5]
-	print key, old_dict[key], retro_dict[key]
+#	print key, old_dict[key], retro_dict[key]
 
-
-problem_children = {}
-for redundant_carrier in retro_dict.keys():
-	for redundant in retro_dict[redundant_carrier]:
-		if redundant in problem_children.keys():
+problem_children = {}	#	Make a dictionary just to list 
+for redundant_carrier in retro_dict.keys():	#	For each individual...
+	for redundant in retro_dict[redundant_carrier]:	#	For each site they're expressing without DB recognition...
+		if redundant in problem_children.keys():	#	Add it to the dict
 			problem_children[redundant].append(redundant_carrier)
 		else:
 			problem_children[redundant] = [redundant_carrier]
 
 #pickle.dump(problem_children, open('lookback_report.pickle','w'))
+#REPORT BACK
 reportback = open('lookback_report.dat','w')
 lens=[]
-print problem_children
 print
 print len(problem_children.keys())
 print len(all_places)
@@ -125,7 +124,6 @@ for i in problem_children.keys():
 	location = curr.fetchone()
 	location_string = '%s:%s-%s' % tuple(location[:-1])
 	reportback.write('%s\t%s\t%s\t%s\n' % tuple([location_string, carrier_string, silent_carrier_string, location[-1]]))
-#location_pk,	list_of_unsighted_carriers
 
 
 print max(lens), min(lens), np.mean(lens), np.median(lens)
