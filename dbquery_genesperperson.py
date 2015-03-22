4a58e2db234f8a73bbcad0acdc196a9a818f005b
Charles Soeder <Charles.Soeder@gmail.com>
2015-03-22 01:31:21 -0400

    updated query scripts for hand curation

--- dbquery_genesperperson.py
+++ dbquery_genesperperson.py
@@ -15,6 +15,18 @@ conn = psycopg2.connect("dbname=denovogenes user=gene password=%s host=bioapps.i
 curr= conn.cursor()
 
 
+def puzzler(all_finds, peeps):
+	person_dict = {}
+	for d00d in peeps:
+		person_dict[d00d[0]] =0
+	for find in all_finds:
+		if find[1] in person_dict.keys():
+			person_dict[find[1]] += 1
+	stats = [max(person_dict.values()), mean(person_dict.values()), median(person_dict.values())]
+	mackses = ''
+	for d00d in person_dict:
+		if person_dict[d00d] == stats[0]:
+			mackses = '%s %s,'%tuple([mackses, d00d])
 
 curr.execute("SELECT find_pk, source, seq, loc FROM find where find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE AND location.lookback_clean is TRUE AND location.handchecked IS TRUE);")
 all_finds = curr.fetchall()
@@ -23,17 +35,7 @@ peeps = curr.fetchall()
 curr.execute("SELECT count(*) FROM location WHERE location.poly IS TRUE AND location.lookback_clean is TRUE AND location.handchecked IS TRUE;")
 numjeanz = curr.fetchone()[0]
 
-person_dict = {}
-for d00d in peeps:
-	person_dict[d00d[0]] =0
-for find in all_finds:
-	if find[1] in person_dict.keys():
-		person_dict[find[1]] += 1
-stats = [max(person_dict.values()), mean(person_dict.values()), median(person_dict.values())]
-mackses = ''
-for d00d in person_dict:
-	if person_dict[d00d] == stats[0]:
-		mackses = '%s %s,'%tuple([mackses, d00d])
+puzzler(all_finds, peeps)
 
 phial = open('Genes_per_person.txt','w')
 phial.write('The %s genes found were distributed across %s individuals.\n'%tuple([numjeanz, len(person_dict.keys())]))
@@ -49,5 +51,42 @@ plt.ylabel('# Individuals')
 plt.xticks(list(arange(0.5, stats[0])), list(arange(stats[0])), rotation=30)
 plt.title('Polymorphic De Novo Genes per Individual')
 plt.savefig('Genes_per_person.png')
+plt.hold(False)
+
+
+curr.execute("SELECT find_pk, source, seq, loc FROM find where find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE ;")
+all_finds = curr.fetchall()
+curr.execute("SELECT person_pk FROM person WHERE person.person_pk IN (SELECT DISTINCT source FROM find WHERE find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE ;")
+peeps = curr.fetchall()
+
+puzzler(all_finds, peeps)
+
+plt.hist(person_dict.values(), bins=stats[0], label='all candidates')
+
+curr.execute("SELECT find_pk, source, seq, loc FROM find where find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE AND location.lookback_clean);")
+all_finds = curr.fetchall()
+curr.execute("SELECT person_pk FROM person WHERE person.person_pk IN (SELECT DISTINCT source FROM find WHERE find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE AND location.lookback_clean)) ;")
+peeps = curr.fetchall()
+
+puzzler(all_finds, peeps)
+plt.hist(person_dict.values(), bins=stats[0], label='never observed to \ninteract with annotations')
+
+curr.execute("SELECT find_pk, source, seq, loc FROM find where find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE AND location.lookback_clean AND location.handchecked IS TRUE);")
+all_finds = curr.fetchall()
+curr.execute("SELECT person_pk FROM person WHERE person.person_pk IN (SELECT DISTINCT source FROM find WHERE find.loc IN (SELECT location_pk FROM location WHERE location.poly IS TRUE AND location.lookback_clean AND location.handchecked IS TRUE)) ;")
+peeps = curr.fetchall()
+
+puzzler(all_finds, peeps)
+plt.hist(person_dict.values(), bins=stats[0], label='hand-curated')
+
+plt.xlabel('Genes per Individual')
+plt.ylabel('# Individuals')
+plt.xticks(list(arange(0.5, stats[0])), list(arange(stats[0])), rotation=30)
+plt.title('Polymorphic De Novo Genes per Individual')
+plt.savefig('Genes_per_person_comparison.png')
+plt.hold()
+
+
+
 
 
