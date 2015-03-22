4839269e9b83c06ebea4cbf84867840fac448c39
Charles Soeder <Charles.Soeder@gmail.com>
2015-03-22 00:08:31 -0400

    now a data file with manual curation data gets loaded into the DB

--- database_manualCurator.py
+++ database_manualCurator.py
@@ -17,7 +17,7 @@ with open('manual_curation.dat', 'rb') as csvfile:
 
 for site in hand_checked:
 	print site
-	curr.execute("SELECT location_pk FROM location WHERE location.start = %s AND location.stop = %s and location.chrom = %s;"%tuple([ site[2], site[3], site[1] ]))
+	curr.execute("SELECT location_pk FROM location WHERE location.start = %s AND location.stop = %s and location.chrom = '%s';"%tuple([ site[2], site[3], site[1] ]))
 	location_pk = curr.fetchone()[0]
 	if site[10] == 'y':	#	if the site has been handchecked and passed.
 		curr.execute("UPDATE location SET handchecked = True WHERE location.location_pk = %s;"%tuple([location_pk]))
